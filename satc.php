b08eb3775c1a34ec2a50e928799f2a25d42a5971
Winter King <winterpk@arr.ae>
2015-02-07 12:21:41 -0700

    Updated

--- satc.php
+++ satc.php
@@ -163,7 +163,6 @@ class satc {
 		$output .= '<ul class="satc-dropdown-menu dropdown-menu" role="menu">';
 		$output .= '<li><a onClick="satcOnClick(this)" href="#" data-format="iCal">Outlook</a></li>';
 		$output .= '<li><a onClick="satcOnClick(this)" href="#" data-format="iCal">iCalendar</a></li>';
-		$output .= "<li><a href=\"http://www.google.com/calendar/event?action=TEMPLATE&text=".$this->valid_fields['name']."&dates=".$this->valid_fields['start_date']."/".$this->valid_fields['end_date']."&details=".$this->valid_fields['description']."&location=[location]&trp=false&sprop=&sprop=name:\" target=\"_blank\" rel=\"nofollow\">Google</a></li>";
 		$output .= '';
 		// <a href="http://www.google.com/calendar/event?
 		//<li><a href=\"http://www.google.com/calendar/event?action=TEMPLATE&text=[event-title]&dates=[start-custom format='Ymd\\THi00\\Z']/[end-custom format='Ymd\\THi00\\Z']&details=[description]&location=[location]&trp=false&sprop=&sprop=name:\" target=\"_blank\" rel=\"nofollow\">Google</a></li>
